c24a5c99a66a97f0015303461cadb514a79ded4d
rajanpatel <rajan2001.patel@gmail.com>
2023-09-16 14:41:04 -0400

    Added controllers and routers

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
-console.log(process.env.NODE_ENV);
-
 connectDB();
 
 app.use(logger);
@@ -26,6 +24,8 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
